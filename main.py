780627a7228d7cda94eea6e7d03ddcb4de6575c4
Sy3dH <hamza.tahir@imaginationai.net>
2025-08-21 16:03:53 +0500

    Added cropping + creation of avatar in the API

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
-from src.routes.routes import router
+from src.routes import crop_avatar_route, create_avatar_route
 import src.configs.settings
 
 app = FastAPI(title="Wordrobe AI APIs")
-app.include_router(router, prefix="/api", tags=["Avatar creation"])
+
+app.include_router(crop_avatar_route.router)
+app.include_router(create_avatar_route.router)
 
 @app.get("/")
 def root():
