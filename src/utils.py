5e95b96b8003d21b98325383887ac3d3f45c1565
Sy3dH <hamza.tahir@imaginationai.net>
2025-08-29 12:23:39 +0500

    Added a util function

--- src/utils.py
+++ src/utils.py
@@ -15,3 +15,12 @@ def get_xy(lm, image_w, image_h):
 def point_in_polygon(point, polygon):
     """Check if point lies inside polygon using cv2"""
     return cv2.pointPolygonTest(np.array(polygon, np.int32), point, False) >= 0
+
+
+def angle_between_points(a, b, c):
+        """Returns angle ABC in degrees."""
+        ba = np.array(a) - np.array(b)
+        bc = np.array(c) - np.array(b)
+        cos_angle = np.dot(ba, bc) / (np.linalg.norm(ba) * np.linalg.norm(bc))
+        cos_angle = np.clip(cos_angle, -1.0, 1.0)
+        return np.degrees(np.arccos(cos_angle))
