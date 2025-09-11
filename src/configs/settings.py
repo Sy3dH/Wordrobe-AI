c599808828ca1bf293cefec215a4aed5e4317acd
Sy3dH <hamza.tahir@imaginationai.net>
2025-09-11 12:36:26 +0500

    Adding FITROOM_API_KEY in config

--- src/configs/settings.py
+++ src/configs/settings.py
@@ -6,7 +6,11 @@ load_dotenv()
 
 # Centralized environment variables
 BFL_API_KEY = os.getenv("BFL_API_KEY")
+FITROOM_API_KEY = os.getenv("FITROOM_API_KEY")
 
 # Optional: Raise error if key is missing
 if not BFL_API_KEY:
     raise RuntimeError("Missing BFL_API_KEY in environment variables.")
+
+if not FITROOM_API_KEY:
+    raise RuntimeError("Missing FITROOM_API_KEY in environment variables.")
